6fba8b568b8dbe2ee42c1f0d16fe9e11b9f8223f
chadcollins <chadrcollins91@gmail.com>
2018-12-31 10:55:03 -0500

    Removed example styles

--- hangman-server/src/Stylesheet.kt
+++ hangman-server/src/Stylesheet.kt
@@ -3,13 +3,5 @@ package com.chadrc.hangman
 import kotlinx.css.*
 
 fun CSSBuilder.mainStyles() {
-    body {
-        backgroundColor = Color.red
-    }
-    p {
-        fontSize = 2.em
-    }
-    rule("p.myclass") {
-        color = Color.blue
-    }
+
 }
