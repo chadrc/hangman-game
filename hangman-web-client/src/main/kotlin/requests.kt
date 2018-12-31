068284cf497e1f09b1c1dc6f8bbc398f22618dc4
chadcollins <chadrcollins91@gmail.com>
2018-12-31 14:29:20 -0500

    Corrected re-enabling button by intercepting callback to request

--- hangman-web-client/src/main/kotlin/requests.kt
+++ hangman-web-client/src/main/kotlin/requests.kt
@@ -3,25 +3,32 @@ import responses.GameResponse
 import kotlin.browser.window
 
 fun <T> makeRequest(path: String, method: String, callback: (T) -> Unit) {
-    val request = Request(path, RequestInit(
-        method = method,
-        cache = RequestCache.NO_CACHE,
-        redirect = RequestRedirect.ERROR
-    ))
+    val request = Request(
+        path, RequestInit(
+            method = method,
+            cache = RequestCache.NO_CACHE,
+            redirect = RequestRedirect.ERROR
+        )
+    )
 
     window.fetch(request).then {
-        when {
-            it.ok -> it.json().then { data -> {
-                console.log("json response", data)
-                callback(data as T)
-            } }
+        when (it.status.toInt()) {
+            200 -> it.json()
+                .then { data -> callback(data as T) }
+                .catch { throwable -> console.error("JSON error", throwable) }
             else -> it.text().then { message -> console.error("Failed request $message") }
         }
     }
 }
 
+fun <T> intercept(func: (T) -> Unit, before: () -> Unit): (T) -> Unit = {
+    before()
+    func(it)
+}
+
 fun startGameRequest(callback: (GameResponse) -> Unit) {
     State.startingGameProp.value = true
-    makeRequest("/start", "POST", callback)
-    State.startingGameProp.value = false
+    makeRequest("/start", "POST", intercept(callback) {
+        State.startingGameProp.value = false
+    })
 }
