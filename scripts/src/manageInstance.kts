34447027c2c195f75ade27b0db00134e9a5533b5
chadcollins <chadrcollins91@gmail.com>
2019-01-05 13:20:01 -0500

    Terminating instance in script

--- scripts/src/manageInstance.kts
+++ scripts/src/manageInstance.kts
@@ -4,6 +4,19 @@
 @file:DependsOn("software.amazon.awssdk:aws-sdk-java:2.2.0")
 
 import software.amazon.awssdk.services.ec2.Ec2Client
+import software.amazon.awssdk.services.ec2.model.TerminateInstancesRequest
 
 val ec2 = Ec2Client.create()!!
+
+val describeInstancesResponse = ec2.describeInstances()!!
+
+val instance = describeInstancesResponse.reservations().flatMap { it.instances() }
+
+val onlyId = instance.first().instanceId()!!
+
+ec2.terminateInstances(TerminateInstancesRequest.builder().apply {
+    instanceIds(onlyId)
+}.build())
+
+println("terminated $onlyId")
 
