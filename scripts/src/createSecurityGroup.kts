03a0024b9253572706e50da0d2e9346aa3532407
chadcollins <chadrcollins91@gmail.com>
2019-01-05 12:49:45 -0500

    Added key pair creation to script

--- scripts/src/createSecurityGroup.kts
+++ scripts/src/createSecurityGroup.kts
@@ -4,10 +4,7 @@
 @file:DependsOn("software.amazon.awssdk:aws-sdk-java:2.2.0")
 
 import software.amazon.awssdk.services.ec2.Ec2Client
-import software.amazon.awssdk.services.ec2.model.AuthorizeSecurityGroupIngressRequest
-import software.amazon.awssdk.services.ec2.model.CreateSecurityGroupRequest
-import software.amazon.awssdk.services.ec2.model.IpPermission
-import software.amazon.awssdk.services.ec2.model.IpRange
+import software.amazon.awssdk.services.ec2.model.*
 
 val ec2 = Ec2Client.create()!!
 
@@ -18,35 +15,44 @@ val createSecurityGroupResponse = ec2.createSecurityGroup(
         .build()
 )!!
 
-val updateSecurityGroupResponse =
-    ec2.authorizeSecurityGroupIngress(
-        AuthorizeSecurityGroupIngressRequest.builder().apply {
-            groupId(createSecurityGroupResponse.groupId())
+ec2.authorizeSecurityGroupIngress(
+    AuthorizeSecurityGroupIngressRequest.builder().apply {
+        groupId(createSecurityGroupResponse.groupId())
 
-            ipPermissions(
-                IpPermission.builder().apply {
-                    ipProtocol("tcp")
-                    fromPort(22)
-                    toPort(22)
-                    ipRanges(
-                        IpRange.builder().apply {
-                            cidrIp("0.0.0.0/0")
-                        }.build()
-                    )
-                }.build(),
+        ipPermissions(
+            IpPermission.builder().apply {
+                ipProtocol("tcp")
+                fromPort(22)
+                toPort(22)
+                ipRanges(
+                    IpRange.builder().apply {
+                        cidrIp("0.0.0.0/0")
+                    }.build()
+                )
+            }.build(),
 
-                IpPermission.builder().apply {
-                    ipProtocol("tcp")
-                    fromPort(3000)
-                    toPort(3000)
-                    ipRanges(
-                        IpRange.builder().apply {
-                            cidrIp("0.0.0.0/0")
-                        }.build()
-                    )
-                }.build()
-            )
-        }.build()
-    )!!
+            IpPermission.builder().apply {
+                ipProtocol("tcp")
+                fromPort(3000)
+                toPort(3000)
+                ipRanges(
+                    IpRange.builder().apply {
+                        cidrIp("0.0.0.0/0")
+                    }.build()
+                )
+            }.build()
+        )
+    }.build()
+)!!
 
 println("Created security group ${createSecurityGroupResponse.groupId()}")
+
+val createKeyPairResponse = ec2.createKeyPair(
+    CreateKeyPairRequest.builder().apply {
+        keyName("TestKeyPair")
+    }.build()
+)!!
+
+println("name: ${createKeyPairResponse.keyName()}")
+println("fingerprint: ${createKeyPairResponse.keyFingerprint()}")
+println("material: ${createKeyPairResponse.keyMaterial()}")
