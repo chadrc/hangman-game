48435d7f6bb34d08631dc4b943843841ba55c743
chadcollins <chadrcollins91@gmail.com>
2019-01-05 13:53:55 -0500

    Now using all direct sub dirs as main srcDirs in scripts Added script to create app ami

---
+++ scripts/createAppAMI/createAppAMI.kts
@@ -0,0 +1,8 @@
+#!/usr/bin/env kscript
+
+@file:CompilerOpts("-jvm-target 1.8")
+@file:DependsOn("software.amazon.awssdk:aws-sdk-java:2.2.0")
+
+import software.amazon.awssdk.services.ec2.Ec2Client
+
+val ec2 = Ec2Client.create()!!
